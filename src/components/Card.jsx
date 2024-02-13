e04bb5fcc1c49cb00807563003cdf9f905620a23
ariel jones dekock <arieljns1@gmail.com>
2024-02-13 14:02:33 +0700

    fixing some bugs that occur when the value is not there

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,118 +6,84 @@ import Step from '@mui/material/Step';
 import StepLabel from '@mui/material/StepLabel';
 import StepContent from '@mui/material/StepContent';
 import Button from '@mui/material/Button';
-import BasicTextFieldComponent from "../utils/BasicTextFieldComponent"
-import TextFieldComponent from "../utils/TextFieldComponent"
-import DatePickerComponent from "../utils/DatePickerComponent"
-import TimePickerComponent from "../utils/TimePickerComponent"
-import CheckBoxComponent from "../utils/CheckBoxComponent"
+import steps from '../data/stepsData';
 import ModalComponent from "../utils/ModalComponent"
 import uploadFormData from '../interaction/upload';
-
-const steps = [
-    {
-        label: 'Nama Karyawan:',
-        description: <TextFieldComponent props="karyawan"/>,
-    },
-    {
-        label: 'Pilih Tanggal',
-        description:
-            <DatePickerComponent />,
-    },
-    {
-        label: 'Kehadiran',
-        description:
-            <CheckBoxComponent props="kehadiran" />,
-    },
-    {
-        label: 'Jam Mulai',
-        description: <TimePickerComponent />,
-    },
-    {
-        label: 'Jam Selesai',
-        description: <TimePickerComponent />,
-    },
-    {
-        label: 'Nama PJK',
-        description: <TextFieldComponent />,
-    },
-    {
-        label: 'Target',
-        description: <BasicTextFieldComponent />,
-    },
-    {
-        label: 'Satuan Target',
-        description: <CheckBoxComponent props="satuanTarget" />,
-    },
-    {
-        label: 'Capaian Target',
-        description: <BasicTextFieldComponent />,
-    },
-    {
-        label: 'Satuan Capaian',
-        description: <CheckBoxComponent props="satuanTarget" />,
-    },
-    {
-        label: 'Deskripsi Kegiatan',
-        description: <BasicTextFieldComponent props="text" />,
-    },
-    {
-        label: 'Apakah Ada Kegiatan Lain?',
-        description: <CheckBoxComponent />,
-    },
-];
+import { useDataContext } from '../hooks/useDataContext';
 
 
-
-export default function Card({ onButtonClick }) {
+export default function Card({ onButtonClick, updateContext }) {
+    const { state, dispatch } = useDataContext();
     const [eventValue, setEventValue] = useState(null);
     const [activeStep, setActiveStep] = useState(0);
     const [isModalOpen, setIsModalOpen] = useState(false);
-    const [context, setContext] = useState({
+
+    // Separate state to store form data
+    const [formData, setFormData] = useState({
         namaKaryawan: "",
         tanggal: {},
         kehadiran: "",
         jamMulai: {},
         jamSelesai: {},
         namaPjk: "",
-        target: 0,
+        target: null,
         satuanTarget: "",
-        capaianTarget: 0,
+        capaianTarget: null,
         satuanCapaian: "",
+        buktiKegiatan: "",
         deskripsiKegiatan: ""
-    })
+    });
 
     const objectKey = ["namaKaryawan", "tanggal", "kehadiran", "jamMulai", "jamSelesai",
         "namaPjk", "target", "satuanTarget", "capaianTarget", "satuanCapaian", "deskripsiKegiatan"
-    ]
+    ];
 
     const updateEventValue = (newEventValue) => {
         setEventValue(newEventValue);
     };
 
     const renderStepContent = (step) => {
-
-        return React.cloneElement(step.description, { updateEventValue, currentContext: context });
+        return React.cloneElement(step.description, { updateEventValue, currentContext: formData });
     };
-    console.log("Ini adalah eventValuenya:", eventValue)
-    const handleNext = () => {
-        const keyIteration = objectKey[activeStep]
-        setContext((prevContext) => ({
-            ...prevContext,
-            [keyIteration]: eventValue,
-        }))
-        setActiveStep((prevActiveStep) => {
-            const nextStep = prevActiveStep + 1;
-            if (nextStep === steps.length || eventValue === "cuti") {
-                uploadFormData(context)
-                setIsModalOpen(true);
-            }
-            return nextStep;
-        });
+
+    console.log("Ini adalah eventValuenya:", eventValue);
+
+    const handleNext = async () => {
+        const keyIteration = objectKey[activeStep];
+        const newFormData = {
+            ...formData,
+            [keyIteration]: eventValue
+        };
+        setFormData(newFormData);
+
+        const nextStep = activeStep + 1;
+        setActiveStep(nextStep);
+
+        if (nextStep === steps.length || eventValue === "cuti") {
+            uploadFormData(newFormData);
+            setIsModalOpen(true);
+        }
+
+        setEventValue(null);
+
         if (eventValue === "ya") {
-            onButtonClick()
+            dispatch({
+                type: 'UPLOADING',
+                payload: newFormData
+            });
+            onButtonClick();
         }
     };
+    function isEmpty(value) {
+        if (typeof value === 'string') {
+            return !value || value.trim().length === 0;
+        } else if (typeof value === 'number') {
+            return !value || value <= 0;
+        } else {
+            return !value;
+        }
+    }
+
 
     const handleBack = () => {
         setActiveStep((prevActiveStep) => prevActiveStep - 1);
@@ -128,23 +94,23 @@ export default function Card({ onButtonClick }) {
         setIsModalOpen(false);
     };
 
-    var testing = activeStep === steps.length || eventValue === "cuti"
+    var testing = activeStep === steps.length || eventValue === "cuti";
+
     return (
         <Box className="form-container" elevation={10} sx={{ maxWidth: 400, position: "relative", right: 5 }}>
             <Stepper elevation={10} activeStep={activeStep} orientation="vertical" >
                 {steps.map((step, index) => (
                     <Step key={step.label}>
-                        <StepLabel
-                            sx={{}}
-                        >
+                        <StepLabel sx={{}}>
                             {step.label}
                         </StepLabel>
                         <StepContent>
                             {renderStepContent(step)}
                             <Box sx={{ mb: 2 }}>
                                 <div>
+
                                     <Button
-                                        disabled={eventValue === null || eventValue.length === 0}
+                                        disabled={isEmpty(eventValue)}
                                         variant="contained"
                                         onClick={handleNext}
                                         sx={{ mt: 1, mr: 1 }}
@@ -170,6 +136,6 @@ export default function Card({ onButtonClick }) {
                 </div>
             )}
         </Box>
-    )
+    );
 }
 
