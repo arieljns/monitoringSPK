9c6e8a4ba7bbdd67e7b8ad3d7f2e84ad7d58c016
ariel jones dekock <arieljns1@gmail.com>
2024-02-13 08:03:36 +0700

    refactoring the card component to make it more modular and following the paradigm of S.O.L.I.D software development lifecycle

---
+++ src/data/stepsData.js
@@ -0,0 +1,61 @@
+import BasicTextFieldComponent from "../utils/BasicTextFieldComponent"
+import TextFieldComponent from "../utils/TextFieldComponent"
+import DatePickerComponent from "../utils/DatePickerComponent"
+import TimePickerComponent from "../utils/TimePickerComponent"
+import CheckBoxComponent from "../utils/CheckBoxComponent"
+
+const steps = [
+    {
+        label: 'Nama Karyawan:',
+        description: <TextFieldComponent props="karyawan" />,
+    },
+    {
+        label: 'Pilih Tanggal',
+        description:
+            <DatePickerComponent />,
+    },
+    {
+        label: 'Kehadiran',
+        description:
+            <CheckBoxComponent props="kehadiran" />,
+    },
+    {
+        label: 'Jam Mulai',
+        description: <TimePickerComponent />,
+    },
+    {
+        label: 'Jam Selesai',
+        description: <TimePickerComponent />,
+    },
+    {
+        label: 'Nama PJK',
+        description: <TextFieldComponent />,
+    },
+    {
+        label: 'Target',
+        description: <BasicTextFieldComponent />,
+    },
+    {
+        label: 'Satuan Target',
+        description: <CheckBoxComponent props="satuanTarget" />,
+    },
+    {
+        label: 'Capaian Target',
+        description: <BasicTextFieldComponent />,
+    },
+    {
+        label: 'Satuan Capaian',
+        description: <CheckBoxComponent props="satuanTarget" />,
+    },
+    {
+        label: 'Deskripsi Kegiatan',
+        description: <BasicTextFieldComponent props="text" />,
+    },
+    {
+        label: 'Apakah Ada Kegiatan Lain?',
+        description: <CheckBoxComponent />,
+    },
+];
+
+
+export default steps
